6f61f9d55b944720e2d4979c08380d4ecb69c7b4
veridelisi <72710290+veridelisi@users.noreply.github.com>
2023-05-04 16:08:57 +0300

    Update AccessModifiers.sol

--- AccessModifiers/src/AccessModifiers.sol
+++ AccessModifiers/src/AccessModifiers.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.13;
 
 // You may modify this contract
 contract Parent {
-    uint256 private _value;
+    uint256 internal _value;
 }
 
 contract Child is Parent {
     /*
         This exercise assumes you understand how access modifiers works.
-        1. `_value` variable is private and can only be accessed by `Parent` contract. Make the variable accessible
+        1. `_value` variable is internal and can only be accessed by `Parent` contract. Make the variable accessible
            to `Parent` and `Child` contract ONLY.
     */
 
