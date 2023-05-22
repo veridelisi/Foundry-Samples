90ef10d45ec34442cb8ad17920fa21fdd3c0dfd5
veridelisi <72710290+veridelisi@users.noreply.github.com>
2023-05-22 23:01:27 +0300

    Update BasicBankV2.sol

--- BasicBankV2/src/BasicBankV2.sol
+++ BasicBankV2/src/BasicBankV2.sol
@@ -6,11 +6,21 @@ contract BasicBankV2 {
     ///     USER    =>  BALANCE
     mapping(address => uint256) public balances;
 
-    /// @notice deposit ether into the contract
+   /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+    function addEther() external payable {
+        balances[msg.sender] += msg.value;
+    }
 
     /// @notice used to withdraw ether from the contract
     /// @param amount of ether to remove. Cannot execeed balance i.e users cannot withdraw more than they deposited
-    function removeEther(uint256 amount) external payable {}
-}
+    function removeEther(uint256 amount) external payable {
+        require (balances[msg.sender] >= amount);
+         balances[msg.sender] -= amount;
+         payable(msg.sender).transfer(amount);
+         //(bool sent,) = msg.sender.call{value: amount}("");
+         //require(sent);
+           }
+           
+           
+             }
