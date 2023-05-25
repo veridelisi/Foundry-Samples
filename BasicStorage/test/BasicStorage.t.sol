54c8e92ec8e1949b64e6c480a3ca526960f911ed
veridelisi <72710290+veridelisi@users.noreply.github.com>
2023-05-25 10:28:20 +0300

    Update BasicStorage.t.sol

--- BasicStorage/test/BasicStorage.t.sol
+++ BasicStorage/test/BasicStorage.t.sol
@@ -14,11 +14,13 @@ contract BasicStorageTest is Test {
     }
 
     function testSetX() external {
+        //changing slot 0
         basicStorage.setX(42);
         //loading slot 0
         uint256 x = uint256(
             vm.load(address(basicStorage), bytes32(uint256(0)))
         );
+        // control slot 0
         assertEq(x, 42, "expected x in storage to be 42 after setX(42)");
     }
 
@@ -27,12 +29,16 @@ contract BasicStorageTest is Test {
         uint256 x = uint256(
             vm.load(address(basicStorage), bytes32(uint256(1)))
         );
+        // control slot 1
         assertEq(x, 43, "expected x in storage to be 42 after setX(42)");
     }
 
    function testGetX() external {
+    //changing slot 0
     basicStorage.setX(49);
+    // getting slot 0
     uint256 x = basicStorage.getX();
+    //control slot 0
     assertEq(x, 49, " x must be 49");
    }
 }
