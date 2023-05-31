10488f423dad02509c75e80a66bed0c906074bec
veridelisi <72710290+veridelisi@users.noreply.github.com>
2023-05-31 23:02:34 +0300

    Update Decoder.sol

--- Decoder/src/Decoder.sol
+++ Decoder/src/Decoder.sol
@@ -2,14 +2,10 @@
 pragma solidity ^0.8.13;
 
 contract Decoder {
-    /* This exercise assumes you know how abi decoding works.
-        1. In the `decodeData` function below, write the logic that decodes a `bytes` data, based on the function parameters
-        2. Return the decoded data
-    */
-    bytes public encoded;
 
-    function decodeData(
-        bytes memory _data
-    ) public pure returns (string memory, uint256) {}
+    function decodeData( bytes memory _data) public pure returns (string memory x , uint256 y) {
+    (x, y) = abi.decode(_data, (string, uint256));
+    
+    }
 }
 
