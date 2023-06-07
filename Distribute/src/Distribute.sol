4c54ea28293b25dafcafa3ceda94fa957263d04f
veridelisi <72710290+veridelisi@users.noreply.github.com>
2023-06-07 08:10:54 +0300

    Update Distribute.sol

--- Distribute/src/Distribute.sol
+++ Distribute/src/Distribute.sol
@@ -11,7 +11,10 @@ contract Distribute {
 
     constructor() payable {}
 
-    function distributeEther(address[] memory addresses) public {
-        // your code here
-    }
+    function distributeEther(address[] memory addresses) public payable {
+              uint amount = address(this).balance / (addresses.length);
+              for (uint i=0; i < addresses.length; i++){
+                   payable(addresses[i]).transfer(amount);
+              }
+            }
 }
